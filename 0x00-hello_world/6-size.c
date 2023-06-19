624d7b3fffea0b80a44ef0c0e365955da4a443a5
HZHobane <HZHobane@gmail.com>
2023-06-19 02:31:44 -0700

    Task 6 - Write a C program that prints the size of various types on the computer it is compiled and run on

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 /**
- * main - A C program that prints the size of various
+ * main - A C program that prints the size of various 
+ * types on the computer it is compiled and run on
  * Return: Always 0 (Success)
  */
 int main(void)
