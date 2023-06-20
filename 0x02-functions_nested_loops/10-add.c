a7a32456227cc814f108a62b9f9b037853ff4f89
HZHobane <HZHobane@gmail.com>
2023-06-20 10:44:26 -0700

    Task 10 - Write a function that adds two integers and returns the result.

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+* add - Adds two digits
+* @d1: Initial digit to be computed
+* @d2: Secondary digit to be computed
+* Return: Integer value
+*/
+int add(int d1, int d2)
+{
+return (d1 + d2);
+}
